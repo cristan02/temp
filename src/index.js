c30a9327db430c1abbc8a6f7f0b8670f89280871
cristan02 <ashbourndcunha@gmail.com>
2024-11-11 23:52:07 +0530

    before call

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-'use strict';
+"use strict";
+
+const _ = require("lodash");
+const { orderBy } = require("lodash/fp");
+const isEqual = require("lodash/isEqual");
 
 module.exports = {
   /**
@@ -16,5 +20,91 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+
+  bootstrap({ strapi }) {
+    const { Server } = require("socket.io");
+    const io = new Server(strapi.server.httpServer, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["userid", "username", "auth"],
+        credentials: false,
+      },
+    });
+
+    io.on("connection", (socket) => {
+      console.log("New client connected");
+      const userid = socket.handshake.headers["userid"];
+      const username = socket.handshake.headers["username"];
+      const auth = socket.handshake.headers["auth"];
+
+      let latestChanges;
+
+      socket.on("get-document", async (id) => {
+        socket.join(id);
+
+        const room = io.sockets.adapter.rooms.get(id);
+        const numClients = room ? room.size : 0;
+
+        //fix below
+        let snapshot = await strapi.db.query("api::snapshot.snapshot").findOne({
+          populate: {
+            doc: {
+              documentId: id,
+            },
+          },
+          orderBy: { createdAt: "desc" },
+        });
+
+        if (numClients === 1) {
+          snapshot = await strapi.documents("api::snapshot.snapshot").create({
+            data: {
+              doc: id,
+              content: snapshot ? snapshot.content : "",
+            },
+          });
+        } else {
+          snapshot = snapshot;
+        }
+        latestChanges = snapshot.content;
+        console.log("snapshot", snapshot.documentId);
+
+        // find from db and send to client
+        socket.emit("load-document", snapshot);
+
+        socket.on("send-changes", (data) => {
+          const { delta, snapshotId, content } = data;
+          socket.broadcast.to(id).emit("receive-changes", delta, id);
+
+          strapi.documents("api::delta.delta").create({
+            data: {
+              doc: id,
+              snapshot: snapshotId,
+              user: userid,
+              content: delta,
+            },
+          });
+        });
+
+        socket.on("save-document", async (data) => {
+          const { content, snapshotId } = data;
+
+          if (!isEqual(latestChanges, content)) {
+            latestChanges = content;
+
+            const entry = await strapi
+              .documents("api::snapshot.snapshot")
+              .update({
+                documentId: snapshot.documentId,
+                data: { content: content },
+              });
+          }
+        });
+
+        socket.on("send-cursor", (data) => {
+          socket.broadcast.to(id).emit("receive-cursor", data, id);
+        });
+      });
+    });
+  },
 };
